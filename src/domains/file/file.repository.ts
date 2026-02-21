3e678a755a5344544ee4e44afcc233aa0df85a5a
kk920106 <kk920106@naver.com>
2026-02-21 10:09:28 +0900

    feat(file-repository): 파일 업로드 요청에 대한 인증 헤더를 추가

--- src/domains/file/file.repository.ts
+++ src/domains/file/file.repository.ts
@@ -1,5 +1,4 @@
-import { ApiResponse } from '@/types/common'
-import { FileUploadResponse } from './file.type'
+import { cookies } from 'next/headers'
 
 const ENDPOINT = '/api/files'
 
@@ -11,10 +10,13 @@ export const fileRepository = {
     const baseURL = process.env.NEXT_PUBLIC_API_URL || ''
     const url = `${baseURL}${ENDPOINT}/v1/upload`
 
+    const cookieStore = await cookies()
+    const accessToken = cookieStore.get('accessToken')?.value
+
     const response = await fetch(url, {
       method: 'POST',
       body: formData,
-      credentials: 'include',
+      headers: accessToken ? { Authorization: `Bearer ${accessToken}` } : undefined,
     })
 
     const status = response.status
