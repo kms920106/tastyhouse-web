c7438f44cf039ab584bdfc3ecc2ef606c5cb65aa
kk920106 <kk920106@naver.com>
2026-02-22 19:59:45 +0900

    refactor(ui): CSS 수정 (AppPrimaryButton.tsx)

--- src/components/ui/AppPrimaryButton.tsx
+++ src/components/ui/AppPrimaryButton.tsx
@@ -1,13 +1,6 @@
-'use client'
-
 import AppButton, { type AppButtonProps } from '@/components/ui/AppButton'
 import { cn } from '@/lib/utils'
 
 export default function AppPrimaryButton({ className, ...props }: AppButtonProps) {
-  return (
-    <AppButton
-      className={cn('text-white bg-[#a91201]', className)}
-      {...props}
-    />
-  )
+  return <AppButton className={cn('bg-[#a91201] text-white', className)} {...props} />
 }
