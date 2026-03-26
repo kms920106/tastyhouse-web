35d42fc7eb1ef88204fd864fd472fe5aafcf0a08
kk920106 <kk920106@naver.com>
2026-03-27 05:36:44 +0900

    feat(ui): 컴포넌트 생성 - AppInpuPhone.tsx

---
+++ src/components/ui/AppInputPhone.tsx
@@ -0,0 +1,41 @@
+import { useRef } from 'react'
+import { cn } from '@/lib/utils'
+import AppInput from './AppInput'
+
+type AppInputPhoneProps = Omit<
+  React.ComponentProps<typeof AppInput>,
+  'type' | 'inputMode' | 'onInput' | 'onPaste'
+>
+
+export default function AppInputPhone({ onChange, ...props }: AppInputPhoneProps) {
+  const ref = useRef<HTMLInputElement>(null)
+
+  const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const input = e.currentTarget
+    const digitsOnly = input.value.replace(/\D/g, '')
+    if (input.value !== digitsOnly) {
+      input.value = digitsOnly
+    }
+    onChange?.({ ...e, target: input, currentTarget: input } as React.ChangeEvent<HTMLInputElement>)
+  }
+
+  const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+    const pastedText = e.clipboardData.getData('text')
+    if (!/^\d+$/.test(pastedText)) {
+      e.preventDefault()
+    }
+  }
+
+  return (
+    <AppInput
+      ref={ref}
+      type="tel"
+      inputMode="numeric"
+      onInput={handleInput}
+      onPaste={handlePaste}
+      {...props}
+      onChange={undefined}
+      className={cn(props.className)}
+    />
+  )
+}
