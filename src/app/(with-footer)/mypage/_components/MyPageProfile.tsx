70e7239038dcffdc23211afa10ce8f4f338d83d6
kk920106 <kk920106@naver.com>
2026-04-18 22:08:33 +0900

    refactor(mypage): PenIcon 컴포넌트 적용

--- src/app/(with-footer)/mypage/_components/MyPageProfile.tsx
+++ src/app/(with-footer)/mypage/_components/MyPageProfile.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import MemberProfileCard from '@/components/member/MemberProfileCard'
+import PenIcon from '@/components/ui/PenIcon'
 import { useMemberProfile } from '@/hooks/useMemberProfile'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function MyPageProfile() {
@@ -13,7 +13,7 @@ export default function MyPageProfile() {
       memberId={memberProfile?.id ?? null}
       editSlot={
         <Link href="/account/profile">
-          <Image src="/images/mypage/icon-pen.png" alt="pencil" width={18} height={16} />
+          <PenIcon />
         </Link>
       }
     />
