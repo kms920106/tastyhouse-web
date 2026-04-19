aef4f80128160a8cfb589b02c77affd5c83c4a86
kk920106 <kk920106@naver.com>
2026-04-19 19:07:57 +0900

    refactor(menu-sidebar): 상태 관리 및 조건부 렌더링을 개선하기 위해 MenuSidebar 컴포넌트의 구조를 재구성

--- src/components/layouts/menu-sidebar/MenuSidebar.tsx
+++ src/components/layouts/menu-sidebar/MenuSidebar.tsx
@@ -2,34 +2,26 @@
 
 import { Sidebar, SidebarContent, SidebarHeader, useSidebar } from '@/components/ui/shadcn/sidebar'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import MenuSidebarBanners from './MenuSidebarBanners'
 import MenuSidebarFoodTypes from './MenuSidebarFoodTypes'
 import MenuSidebarProfile from './MenuSidebarProfile'
 
-export default function MenuSidebar() {
+function MenuSidebarContent() {
   const { setOpenMobile } = useSidebar()
 
-  const handleClose = () => {
-    setOpenMobile(false)
-  }
-
   return (
-    <Sidebar side="left" collapsible="offcanvas">
+    <>
       <SidebarHeader className="gap-0 p-0">
         <div className="flex items-center justify-end gap-[18px] p-[15px]">
-          {/* 설정 버튼 */}
           <button className="flex items-center justify-center w-[18px] h-[18px]">
             <Image src="/images/icon-setting-black.png" alt="설정" width={18} height={18} />
           </button>
-
-          {/* 검색 버튼 */}
           <button className="flex items-center justify-center w-[18px] h-[18px]">
             <Image src="/images/icon-search.png" alt="검색" width={18} height={18} />
           </button>
-
-          {/* 닫기 버튼 */}
           <button
-            onClick={handleClose}
+            onClick={() => setOpenMobile(false)}
             className="flex items-center justify-center w-[18px] h-[18px]"
           >
             <Image src="/images/icon-close.png" alt="닫기" width={16} height={16} />
@@ -37,15 +29,27 @@ export default function MenuSidebar() {
         </div>
       </SidebarHeader>
       <SidebarContent className="p-0 flex flex-col overflow-y-auto">
-        {/* 프로필 */}
         <MenuSidebarProfile />
-
-        {/* 음식 종류 */}
         <MenuSidebarFoodTypes />
-
-        {/* 배너 */}
         <MenuSidebarBanners />
       </SidebarContent>
+    </>
+  )
+}
+
+export default function MenuSidebar() {
+  const { open, openMobile } = useSidebar()
+  const [hasOpened, setHasOpened] = useState(false)
+
+  useEffect(() => {
+    if (open || openMobile) {
+      setHasOpened(true)
+    }
+  }, [open, openMobile])
+
+  return (
+    <Sidebar side="left" collapsible="offcanvas">
+      {hasOpened && <MenuSidebarContent />}
     </Sidebar>
   )
 }
