0f877976643ee1763376176a4afb8f59ab05de16
kk920106 <kk920106@naver.com>
2026-05-03 23:40:07 +0900

    refactor(privacy): 로딩 환경 개선을 위해 FetchErrorState를 Suspense 및 PrivacyHtmlContent로 대체

--- src/app/privacy/_components/PrivacyContent.tsx
+++ src/app/privacy/_components/PrivacyContent.tsx
@@ -1,24 +1,13 @@
-import FetchErrorState from '@/components/ui/FetchErrorState'
-import HtmlContent from '@/components/ui/HtmlContent'
-import { policiesRepository } from '@/domains/policies/policies.repository'
-import { COMMON_ERROR_MESSAGES } from '@/lib/constants'
+import HtmlContentSkeleton from '@/components/ui/HtmlContentSkeleton'
+import { Suspense } from 'react'
+import PrivacyHtmlContent from './PrivacyHtmlContent'
 
 export default async function PrivacyContent() {
-  const { error, status, data } = await policiesRepository.getLatestPrivacyPolicy()
-
-  if ((error && status === 404) || !data) {
-    return <FetchErrorState message={COMMON_ERROR_MESSAGES.FETCH_ERROR('개인정보처리방침')} />
-  }
-
-  if (error) {
-    return <FetchErrorState message={COMMON_ERROR_MESSAGES.API_FETCH_ERROR} />
-  }
-
-  const { content } = data
-
   return (
     <div className="px-[15px] py-7">
-      <HtmlContent content={content} />
+      <Suspense fallback={<HtmlContentSkeleton />}>
+        <PrivacyHtmlContent />
+      </Suspense>
     </div>
   )
 }
---
+++ src/app/privacy/_components/PrivacyHtmlContent.tsx
@@ -0,0 +1,20 @@
+import FetchErrorState from '@/components/ui/FetchErrorState'
+import HtmlContent from '@/components/ui/HtmlContent'
+import { policiesRepository } from '@/domains/policies/policies.repository'
+import { COMMON_ERROR_MESSAGES } from '@/lib/constants'
+
+export default async function PrivacyHtmlContent() {
+  const { error, status, data } = await policiesRepository.getLatestPrivacyPolicy()
+
+  if ((error && status === 404) || !data) {
+    return <FetchErrorState message={COMMON_ERROR_MESSAGES.FETCH_ERROR('개인정보처리방침')} />
+  }
+
+  if (error) {
+    return <FetchErrorState message={COMMON_ERROR_MESSAGES.API_FETCH_ERROR} />
+  }
+
+  const { content } = data
+
+  return <HtmlContent content={content} />
+}
