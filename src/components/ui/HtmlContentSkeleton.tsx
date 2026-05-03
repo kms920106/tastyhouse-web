a529e3967b90fba9ffd84422bbf291a3ce0f6998
kk920106 <kk920106@naver.com>
2026-05-03 23:36:28 +0900

    feat(ui): 로딩 상태 표현을 개선하기 위해 HtmlContentSkeleton 컴포넌트를 추가

--- src/components/ui/HtmlContentSkeleton.tsx
+++ src/components/ui/HtmlContentSkeleton.tsx
@@ -1,8 +1,8 @@
 import { Skeleton } from '@/components/ui/shadcn/skeleton'
 
-export default function TermsContentSkeleton() {
+export default function HtmlContentSkeleton() {
   return (
-    <div className="px-[15px] py-7 flex flex-col gap-3">
+    <div className="flex flex-col gap-3">
       <Skeleton className="h-3.5 w-1/3" />
       <Skeleton className="h-3.5 w-full" />
       <Skeleton className="h-3.5 w-full" />
