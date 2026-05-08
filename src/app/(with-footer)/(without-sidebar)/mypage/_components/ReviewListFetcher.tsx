bce4c408184b4e44d7fb01ca91650c2c51d9826e
kk920106 <kk920106@naver.com>
2026-05-09 06:48:59 +0900

    refactor(review): ReviewListFetcher에서 데이터 구조를 분해하여 가독성과 유지보수성을 향상

--- src/app/(with-footer)/(without-sidebar)/mypage/_components/ReviewListFetcher.tsx
+++ src/app/(with-footer)/(without-sidebar)/mypage/_components/ReviewListFetcher.tsx
@@ -27,5 +27,7 @@ export default function ReviewListFetcher() {
     return <FetchErrorState message={COMMON_ERROR_MESSAGES.FETCH_ERROR('리뷰')} />
   }
 
-  return <ReviewList reviews={data.reviews} hasMoreReviews={data.hasMore} />
+  const { reviews, hasMore } = data
+
+  return <ReviewList reviews={reviews} hasMoreReviews={hasMore} />
 }
