e13d2bbe35d081b6156e65196f0efdf32ac1772f
kk920106 <kk920106@naver.com>
2026-05-15 22:20:45 +0900

    refactor(ui): 코드 간소화를 위해 FixedBottomSection 컴포넌트에서 사용되지 않는 className 속성을 제거

--- src/components/ui/FixedBottomSection.tsx
+++ src/components/ui/FixedBottomSection.tsx
@@ -1,16 +1,12 @@
-import { cn } from '@/lib/utils'
 import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
-  className?: string
 }
 
-export default function FixedBottomSection({ children, className = '' }: Props) {
+export default function FixedBottomSection({ children }: Props) {
   return (
-    <section
-      className={cn('fixed bottom-0 left-0 right-0 bg-white border-t border-[#eeeeee] box-border', className)}
-    >
+    <section className="fixed bottom-0 left-0 right-0 bg-white border-t border-[#eeeeee] box-border">
       {children}
     </section>
   )
