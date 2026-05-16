f2065c220cd1a2069638b4152a1515732a80206d
kk920106 <kk920106@naver.com>
2026-05-16 15:11:41 +0900

    refactor(search): 코드 구성을 개선하기 위해 PopularKeywordListItem 컴포넌트를 추출

---
+++ src/app/search/_components/PopularKeywordListItem.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils'
+
+interface Props {
+  rank: number
+  keyword: string
+  isNew: boolean
+}
+
+export default function PopularKeywordListItem({ rank, keyword, isNew }: Props) {
+  return (
+    <div className="flex items-center gap-5">
+      <span className={cn('w-5 text-sm font-normal text-right shrink-0', rank <= 3 && 'text-main')}>
+        {rank}
+      </span>
+      <span className="flex-1 text-sm leading-[16px] font-regular">{keyword}</span>
+      {isNew && <span className="text-sm leading-[16px] text-main">NEW</span>}
+    </div>
+  )
+}
