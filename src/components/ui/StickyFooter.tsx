b637447485abf97aa2bfb15656d1997d234ebf05
kk920106 <kk920106@naver.com>
2026-05-24 15:08:57 +0900

    fix(ui): 적절한 스태킹 컨텍스트를 위해 StickyFooter에 z-index를 추가

--- src/components/ui/StickyFooter.tsx
+++ src/components/ui/StickyFooter.tsx
@@ -8,7 +8,7 @@ export default function StickyFooter({ children }: Props) {
   return (
     <>
       <div className="h-[70px]" aria-hidden="true" />
-      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-[#eeeeee] box-border">
+      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-[#eeeeee] box-border z-1">
         {children}
       </div>
     </>
