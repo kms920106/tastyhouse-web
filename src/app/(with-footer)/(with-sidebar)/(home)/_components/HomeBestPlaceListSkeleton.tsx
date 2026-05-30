44c8f6eedff3695d5718cc6202d0141d97ac8679
kk920106 <kk920106@naver.com>
2026-05-30 23:24:20 +0900

    refactor(skeletons): HomeBestPlaceListSkeleton 및 HomeTodayDiscountProductListSkeleton 컴포넌트의 레이아웃과 스타일을 업데이트

--- src/app/(with-footer)/(with-sidebar)/(home)/_components/HomeBestPlaceListSkeleton.tsx
+++ src/app/(with-footer)/(with-sidebar)/(home)/_components/HomeBestPlaceListSkeleton.tsx
@@ -2,7 +2,7 @@ import { PlaceCardSkeleton } from '@/components/places/PlaceCardSkeleton'
 
 export function HomeBestPlaceListSkeleton() {
   return (
-    <div className="grid grid-cols-2 gap-3 mb-[25px]">
+    <div className="grid grid-cols-2 gap-x-[15px] gap-y-10 mb-10">
       {[...Array(4)].map((_, i) => (
         <PlaceCardSkeleton key={i} />
       ))}
--- src/app/(with-footer)/(with-sidebar)/(home)/_components/HomeTodayDiscountProductListSkeleton.tsx
+++ src/app/(with-footer)/(with-sidebar)/(home)/_components/HomeTodayDiscountProductListSkeleton.tsx
@@ -5,13 +5,11 @@ const COUNT = 4
 
 export function HomeTodayDiscountProductListSkeleton() {
   return (
-    <div className="mb-10">
+    <div className="border-t border-b border-[#eeeeee] py-[15px]">
       {Array.from({ length: COUNT }).map((_, i) => (
         <Fragment key={i}>
           <DiscountProductItemSkeleton />
-          {i < COUNT - 1 && (
-            <div className="border-t border-[#eeeeee] my-[15px]" />
-          )}
+          {i < COUNT - 1 && <div className="border-t border-[#eeeeee] my-[15px]" />}
         </Fragment>
       ))}
     </div>
